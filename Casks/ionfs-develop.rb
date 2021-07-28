271f82ef4f12f96b87b52ca4ab6af6c28f1a2269
Josh Fraser <1416487-jishf@users.noreply.gitlab.com>
2021-07-28 22:22:37 +0000

    add conditional check to develop cask for amd64/arm64

--- Casks/ionfs-develop.rb
+++ Casks/ionfs-develop.rb
@@ -1,9 +1,13 @@
 cask "ionfs-develop" do
-    version "0.2.4-develop.1"
-    sha256 "469ce010e05be9cddcaad6e448eab812b0120c09c04a49b64c437275ac2a7e80"
+    version "0.2.5-develop.1"
   
-    url "https://ionfs.s3.fr-par.scw.cloud/develop/ionfs-#{version}-mac.tar.bz2",
-        verified: "github.com/ionburstcloud/ionfs"
+  if Hardware::CPU.intel?
+    sha256 "c33e948ce8380984205c9fef552b3c3888b8189b4be710d476937babe12992e0"
+    url "https://ionfs.s3.fr-par.scw.cloud/develop/ionfs-#{version}-mac.tar.bz2"
+  else
+    sha256 "2311ea591c4cad518620fc83284b67e89b0a922ff715b18881f5347c2daac047"
+    url "https://ionfs.s3.fr-par.scw.cloud/develop/ionfs-#{version}-mac-arm64.tar.bz2"
+  end   
     name "IonFS CLI (develop branch)"
     desc "IonFS Command Line Interface provides a set of tools to manage data stored by Ionburst Cloud as if it were a remote filesystem."
     homepage "https://ionburst.cloud/docs/ionfs"
