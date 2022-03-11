59eb399ef8f17397744b3bd5f1fa3416283d5002
Josh Fraser <josh@dhcp.sucks>
2022-03-11 11:05:25 +0000

    update ionfs-develop to 0.3.0-develop.4

--- Casks/ionfs-develop.rb
+++ Casks/ionfs-develop.rb
@@ -1,11 +1,11 @@
 cask "ionfs-develop" do
-    version "0.3.0-develop.3"
+    version "0.3.0-develop.4"
   
   if Hardware::CPU.intel?
-    sha256 "cb8450f03a3885db3486742b681b43c5cd9582cbc836f68dfa396be93b63c399"
+    sha256 "775ae32372e30cd853d89aebdf102c081173c74918c52d093e5c45e128196bc2"
     url "https://ionfs.s3.fr-par.scw.cloud/develop/ionfs-#{version}-mac.tar.bz2"
   else
-    sha256 "74ef2dfd8839c3dca8135d4a70134ecf98b5ebf0a5795b3d9e838ad8236cacf7"
+    sha256 "df159730331dccdda2f8764176ca9f6327bf079270c509bd9ce1314b93a392a3"
     url "https://ionfs.s3.fr-par.scw.cloud/develop/ionfs-#{version}-mac-arm64.tar.bz2"
   end   
     name "IonFS CLI (develop branch)"
